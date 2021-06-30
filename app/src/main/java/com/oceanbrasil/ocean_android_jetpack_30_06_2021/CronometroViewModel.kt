534e2378bb443cbfa28e9d7961cc4fc70e781bc8
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-30 12:22:01 -0300

    Create CronometroViewModel.kt

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_jetpack_30_06_2021/CronometroViewModel.kt
@@ -0,0 +1,23 @@
+package com.oceanbrasil.ocean_android_jetpack_30_06_2021
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class CronometroViewModel : ViewModel() {
+    val elapsedTime = MutableLiveData<Long>(0)
+
+    private val timer = Timer()
+
+    init {
+        timer.scheduleAtFixedRate(
+            timerTask {
+                // Ação a ser executada
+                val value = elapsedTime.value ?: 0
+
+                elapsedTime.value = value + 1
+            }, 1000, 1000
+        )
+    }
+}
