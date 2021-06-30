779910952f484fd3083c8c110d7a5f7f93a1089d
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-30 10:42:44 -0300

    Exemplo de saveInstanceState

--- app/src/main/java/com/oceanbrasil/ocean_android_jetpack_30_06_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_jetpack_30_06_2021/MainActivity.kt
@@ -2,10 +2,49 @@ package com.oceanbrasil.ocean_android_jetpack_30_06_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        Log.d("MainActivity", "onCreate")
+//
+//        val textView = findViewById<TextView>(R.id.textView)
+//        textView.setOnClickListener {
+//            textView.text = "Elemento clicado!"
+//        }
+//    }
+//
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//
+//        Log.d("MainActivity", "onSave Instance State")
+//
+//        val textView = findViewById<TextView>(R.id.textView)
+//        outState.putString("VALOR_TEXT_VIEW", textView.text.toString())
+//    }
+//
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//
+//        Log.d("MainActivity", "onRestore Instance State")
+//
+//        val textView = findViewById<TextView>(R.id.textView)
+//        val valorTextView = savedInstanceState.getString("VALOR_TEXT_VIEW")
+//        textView.text = valorTextView
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//
+//        Log.d("MainActivity", "Activity destruída!")
+//    }
 }
